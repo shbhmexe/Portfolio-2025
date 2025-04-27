af33216bfd8912fe039108451660ee74fff20a2f
Shubham shukla <shubhushukla586@gmail.com>
2025-04-27 13:14:14 +0530

    new
    new

--- Portfolio/app/components/canvas/Ball.tsx
+++ Portfolio/app/components/canvas/Ball.tsx
@@ -1,37 +1,17 @@
 'use client';
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
+import { OrbitControls, Preload } from "@react-three/drei";
 
 const Ball = ({ imgUrl }: { imgUrl: string }) => {
-  const [decal] = useTexture([imgUrl]);
-
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-        />
-      </mesh>
-    </Float>
+    <mesh castShadow receiveShadow scale={2.75}>
+      <icosahedronGeometry args={[1, 1]} />
+      <meshStandardMaterial
+        color="#fff8eb"
+        flatShading
+      />
+    </mesh>
   );
 };
 
@@ -43,6 +23,8 @@ const BallCanvas = ({ icon }: { icon: string }) => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<div className="canvas-loader" />}>
+        <ambientLight intensity={0.25} />
+        <directionalLight position={[0, 0, 0.05]} />
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
       </Suspense>
