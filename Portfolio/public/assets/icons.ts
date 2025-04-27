076da6c447ae80b752987c43d38eb63dc0c27027
Shubham shukla <shubhushukla586@gmail.com>
2025-04-27 12:47:20 +0530

    Portfolio
    new

---
+++ Portfolio/public/assets/icons.ts
@@ -0,0 +1,5 @@
+import github from "./github.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export { github, menu, close }; 
---
+++ Portfolio/public/assets/index.ts
@@ -0,0 +1,56 @@
+// Define asset paths as strings instead of imports
+// Technology icons
+const tech = {
+  html: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+  css: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+  javascript: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  typescript: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+  reactjs: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+  redux: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+  tailwind: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg',
+  nodejs: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+  mongodb: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
+  threejs: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/threejs/threejs-original.svg',
+  git: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+  figma: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
+  docker: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+};
+
+// Company icons
+const company = {
+  meta: '/codexintern_logo.jpg',
+  shopify: '/1732615135776.jpg',
+  starbucks: '/hacktoberfest_logo.jpg',
+  tesla: '/1680591532979.jpg',
+  // google: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png',
+  // freelance: '/1732615135776.jpg',
+};
+
+// Project images
+const projects = {
+  carrent: 'https://camo.githubusercontent.com/1a2432623a5aad4521a2eebd3bc5a7161771804d95c8c96dc5583adb8a32c285/68747470733a2f2f692e6962622e636f2f64344a465771542f53637265656e73686f742d323032332d30332d31352d61742d322d33392d30322d414d2e706e67',
+  jobit: 'https://camo.githubusercontent.com/a0ab7f4896097bed2c3bd09b98e690587a339b1ba0e2d25bf70e0242dbd13932/68747470733a2f2f692e6962622e636f2f6262376a4b44742f53637265656e73686f742d323032332d30332d31352d61742d322d34302d34362d414d2e706e67',
+  tripguide: 'https://camo.githubusercontent.com/f21f39fa9cd6c8cda817abfa4da7257bc79dada49e3eb1b5faa32fd601d10a6e/68747470733a2f2f692e6962622e636f2f564a6d4d4678592f53637265656e73686f742d323032332d30332d31352d61742d322d33372d33392d414d2e706e67',
+};
+
+// Service icons
+const service = {
+  web: 'https://cdn-icons-png.flaticon.com/512/2537/2537338.png',
+  mobile: 'https://cdn-icons-png.flaticon.com/512/4616/4616184.png',
+  backend: 'https://cdn-icons-png.flaticon.com/512/2906/2906274.png',
+  creator: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+};
+
+// Hero section
+const herobg = 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+const logo = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg';
+
+// Export all assets
+export {
+  logo,
+  herobg,
+  service,
+  tech,
+  company,
+  projects
+}; 
